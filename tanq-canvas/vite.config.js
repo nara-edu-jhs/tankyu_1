d4476b258b51345aedb8a78f375e4512d4716f16
nara-edu-jhs <seikatsubu.nkf@gmail.com>
2026-06-16 16:08:19 +0900

    Delete tanq-canvas directory

--- tanq-canvas/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // 👈 これがあることで、GitHub PagesでURLを開いたときに画面が真っ白になるのを防ぎます！
-})
