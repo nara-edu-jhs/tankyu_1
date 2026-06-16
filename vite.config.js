edc24eac4ef78ca34b3e3fc15d3feb67f5eebf24
nara-edu-jhs <seikatsubu.nkf@gmail.com>
2026-06-16 16:12:51 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 これがあることで、GitHub PagesでURLを開いたときに画面が真っ白になるのを防ぎます！
+  base: '/tankyu_1/', // 👈 ここを「/リポジトリ名/」に変更します！
 })
